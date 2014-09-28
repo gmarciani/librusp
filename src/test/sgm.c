3809d998af2c5eca157712100257994f06f54401
giacomomarciani <giacomo.marciani@gmail.com>
2014-09-28 16:53:13 +0200

    RUDP Segment enhanced!

--- src/test/sgm.c
+++ src/test/sgm.c
@@ -1,12 +1,14 @@
 #include "../core/rudpsegment.h"
 
 int main(void) {
+	Stream stream;
 	Segment sgmOne, sgmTwo;
 	char *strsgm = NULL;
 	char *ssgm = NULL;
+	char *msg = "Hello World! I'm happy! Stay hungry, stay foolish, folks!";
 	int i;
 
-	puts("# Creating SYNACK segments with urgp=5 wnds=324, seqn=51, ackn=22, pld=Hello World! #");
+	puts("# Creating SYNACK segments (MAX_PLD=5) with urgp=5 wnds=324, seqn=51, ackn=22, pld=Hello World! #");
 
 	sgmOne = createSegment(RUDP_SYN | RUDP_ACK, 5, 324, 51, 22, "Hello world!");
 
@@ -48,6 +50,22 @@ int main(void) {
 	} else {
 		puts("Header match FAILURE");
 	}
+
+	puts("# Stream of Segments (MAX_PLD=5) with message=Hello World! I'm happy! Stay hungry, stay foolish, folks! #");
+
+	stream = createStream(msg);
+
+	printf("Message len: %zd Stream size: %lu Stream len: %lu\n", strlen(msg), stream.size, stream.len);
+
+	for (i = 0; i < stream.size; i++) {
+		strsgm = segmentToString(stream.segments[i]);
+		printf("%s\n", strsgm);
+		free(strsgm);
+	}
+
+	puts("# Freeing stream of segments #");
+
+	freeStream(&stream);
 
 	exit(EXIT_SUCCESS);
 }
