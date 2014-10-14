c6972562703356d730773367370143f1c6f35c63
giacomomarciani <giacomo.marciani@gmail.com>
2014-10-14 22:18:43 +0200

    Macro utilities

---
+++ src/util/macroutil.h
@@ -0,0 +1,16 @@
+#ifndef MACROUTIL_H_
+#define MACROUTIL_H_
+
+#ifndef ERREXIT
+#define ERREXIT(format, ...) do{fprintf(stderr, format "\n", ##__VA_ARGS__);exit(EXIT_FAILURE);}while(0)
+#endif
+
+#ifndef DBGPRINT
+#define DBGPRINT(debug, format, ...) if ((debug)) fprintf(stdout, format "\n", ##__VA_ARGS__)
+#endif
+
+#ifndef DBGFUNC
+#define DBGFUNC(debug, function) if ((debug)) (function)
+#endif
+
+#endif /* MACROUTIL_H_ */
